9c9f9bf62e61cf1113e68433f87d5a4d68cd7a7e
Luis Fernando Varón Acosta <luih.pm.14@gmail.com>
2023-05-15 20:23:41 -0500

    Modified the base template and the login view form

--- PetAdoptionHouse/index/urls.py
+++ PetAdoptionHouse/index/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("login", views.login, name="login"),
+    path("login", views.login_view, name="login"),
     path("home", views.home, name="Home"),
     path("catalogue", views.catalogue, name="Catalogue"),
     path("pet", views.petmain, name="Pet"),
--- PetAdoptionHouse/index/views.py
+++ PetAdoptionHouse/index/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-def login(request):
-    return render(request, "index/login.html")
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        try:   
+            if form.is_valid():
+                email = form.cleaned_data.get('email')
+                password = form.cleaned_data.get('password')
+                user = authenticate(request, username=email, password=password)
+                if user is not None:
+                    login(request, user)
+                    return redirect('catalogue')
+        except Exception as e:
+            erorr_message = "Error: "+str(e)
+            return render(request, 'index/login.html', {'form':form, 'error_message': erorr_message})
+    else:
+        form = AuthenticationForm()        
+    return render(request, "index/login.html", {'form':form})
 
 def home(request):
     return HttpResponse(f"Home!!!")
