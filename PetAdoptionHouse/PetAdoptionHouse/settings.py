ac7d3ffac21d491e15fa66543b16b7a7fcbadeb6
WeedoPC <luih.pm.14@gmail.com>
2023-11-16 15:42:30 -0500

    Fixing admin panel without styles

--- PetAdoptionHouse/PetAdoptionHouse/settings.py
+++ PetAdoptionHouse/PetAdoptionHouse/settings.py
@@ -135,8 +135,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'index.User'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'index/templates'),
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'index/templates')
 ]
 
 LOGOUT_REDIRECT_URL = 'login'
