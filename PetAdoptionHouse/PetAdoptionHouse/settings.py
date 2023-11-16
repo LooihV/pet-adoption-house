e50a1096bd76c4aac3ee4679a217638dc6d2d25a
WeedoPC <luih.pm.14@gmail.com>
2023-11-15 19:53:21 -0500

    Allowed host config

--- PetAdoptionHouse/PetAdoptionHouse/settings.py
+++ PetAdoptionHouse/PetAdoptionHouse/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(40pw#deg5+y%!gdld*$1)yi7gyl12taosat#)o7+=+b*dfkej
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['weedodev.pythonanywhere.com']
 
 
 # Application definition
